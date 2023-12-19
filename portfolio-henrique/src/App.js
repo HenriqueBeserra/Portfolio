f4ae477c8ec7bbd1b79791bec0df7234867f440c
Henrique Beserra <millerhiquinho@gmail.com>
2023-12-19 01:20:59 -0300

    Inicialização do projeto de um portfólio com react.js

---
+++ portfolio-henrique/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeaderBanner from './components/atoms/HeaderBanner';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderBanner />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-henrique/src/components/atoms/HeaderBanner.js
@@ -0,0 +1,9 @@
+import Banner from '../../assets/images/banner_header.png'
+export default function Header_banner () {
+    return (
+        <div>
+            <img src={Banner} alt='banner astronalta desenvolvedor'></img>
+        </div>
+    );
+
+}
